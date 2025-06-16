dbee2d1c97af448ee1e5b321338fadc5fde7b527
NahuelPerrone85 <nahuelperrone85@gmail.com>
2025-06-16 13:41:33 +0000

    Trabajo Terminado

--- src/js/components/List.jsx
+++ src/js/components/List.jsx
@@ -14,6 +14,7 @@ const List = () => {
         function borrar (e){
              e.preventDefault()
                 agregar(e)
+                setTarea("")
             }
 
         const borrarTarea = (Delete) => {
@@ -26,35 +27,27 @@ const List = () => {
     return(
         <div className="conteiner bg-secondary-light  m-auto w-100 vh-100">
          <h1 className="text-center text-secondary pt-3">todos</h1>
-        <div className= "sombra flex-direction-column w-50 m-auto bg-white p-3 rounded-1">
-        <form onSubmit = {borrar}>
-         <input type="text" name="valor" value={tarea} onkeyDown={intro} onChange={(e)=>{setTarea(e.target.value)}} className="form-control rounded-0 border-0 border-bottom ps-1" placeholder="Añadir Tareas"/>
+        <div className= "sombra flex-direction-column w-50  m-auto bg-white pb-3 rounded-1">
+         <div className= "sombra flex-direction-column h-50 m-auto bg-white p-3 rounded-1">
 
+             <form onSubmit = {borrar}>
+                 <input type="text" name="valor" value={tarea} 
+                     onkeyDown={intro} onChange={(e)=>{setTarea(e.target.value)}} 
+                     className="form-control rounded-0 border-0 border-bottom ps-1" placeholder="Add Task"/>
+             </form>
                  {tareas.map((t, index) => (
-                     <div key={index} className="elemento border-bottom d-flex justify-content-between align-items-center">
-                         <p className="m-1 ">{t}</p>
-                         <i className="bi bi-x-circle text-danger"
-                             onClick={() => borrarTarea(index)}
-                             ></i>
-                    </div>
-                ))}
-         </form>
-         
-
-        <div>
-         <p className="mt-2 text-secondary"> {tareas.length} item left </p>
+                   <div key={index} className="elemento border-bottom d-flex justify-content-between align-items-center m-1">    
+                     <p  className="m-1">{t}</p>
+                     <i className="bi bi-x-circle text-danger"
+                        onClick={() => borrarTarea(index)}></i>
+                   </div>
+                 ))}
+                 <p className="mt-2 text-secondary"> {tareas.length} item left </p>
+         </div>
         </div>
-        </div>
-
         </div>
 
     )
 };
 export default List
 
-
-
-
-//   const formulario = (e)=> {
-//         setValor({...valor,[e.target.valor] : e.target.valor});
-//     }
